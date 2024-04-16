e5bde744fb320484646fe0e58bab9c21d96cc92a
MertKirkar <0mertkirkar@gmail.com>
2024-04-16 13:28:45 +0300

    Todo Updated

--- script.js
+++ script.js
@@ -2,27 +2,49 @@ const ekle=document.querySelector("#button-addon2")
 const searchInput=document.querySelector("#headerInput")
 const list=document.querySelector("#list")
 const lis=document.querySelectorAll("ul")
+const closeTag="X"
+
 
 ekle.addEventListener("click",handleAdd)
 
 function handleAdd() {
   
     const newLi=document.createElement("li")
-    const classList=["bg-slate-300","break-words"]
+    const closeButton=document.createElement("button")
+    const classList=["bg-slate-300","break-words","flex","justify-between"]
    
-    newLi.textContent=` ${searchInput.value} `
+    newLi.textContent=` ${searchInput.value}`
+    closeButton.textContent=`${closeTag}`
     
-   if(searchInput.value!=0){
-    list.appendChild(newLi)
+  
+    
+   if(searchInput.value.trim() !== ""){
+    newLi.appendChild(closeButton)
+    list.appendChild(newLi) 
+    
     newLi.classList.add(...classList)
    }else{
     alert("Boş To do Girilemez..! Lütfen Bir şeyler yazınız...")
    }
 
+   
+
+   closeButton.addEventListener("click",()=>{
+    list.removeChild(newLi)
+   })
+
+   
+
 }
+
 lis.forEach(eleman=>{
     eleman.addEventListener("click",e=>{
-       e.target.classList.toggle("line-through")
+      if(e.target.tagName=="LI") {
+        e.target.classList.toggle("line-through")
+        e.target.classList.toggle("bg-slate-700")
+        e.target.classList.toggle("text-white")
+            
+      }
        
     })
     
